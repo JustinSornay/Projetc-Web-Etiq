d9b17074b9bd69b577120256a240b90c0f7bbc10
JulienPirat <72934794+JulienPirat@users.noreply.github.com>
2020-11-22 18:34:50 +0100

    Update login.php

--- Main PHP/login.php
+++ Main PHP/login.php
@@ -1,13 +1,5 @@
 <?php
-try{
-
-
-$pdo = new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8', 'root', '');
-}
-catch(Exception $e)
-{
-    die('Erreur : ' . $e->getMessage());
-}
+include 'Connecbdd.php';
 
 
   $Mail = $_POST['Mail'];
